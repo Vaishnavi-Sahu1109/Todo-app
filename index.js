e631cbd1f6ed8b0faac1792cecdbbb67bfa0b835
Vaishnavi Sahu <sahuvaishnavi1109@gmail.com>
2024-01-01 00:43:27 +0530

    completed todo app

--- index.js
+++ index.js
@@ -1,14 +1,60 @@
-const inputText = document.getElementById("todo-input")
-const button = document.getElementsByTagName("button")[0]
-const list = document.getElementById("list")
+const inputElement = document.getElementById("todo-input");
+const button = document.getElementsByTagName("button")[0];
+const list = document.getElementById("list");
+const todoBottom = document.getElementById("todo-bottom");
 
-button.addEventListener("click", ()=>{
-  const text = inputText.value
+const isEditing = false;
+const editedTodo = "";
 
-  const li = document.createElement("li")
-  li.innerText = text 
-  list.appendChild(li)
-  
-  
-})
+function handleSubmit(event) {
+  event.preventDefault();
 
+  const text = inputElement.value;
+  if (!text) {
+    return;
+  }
+
+  const li = document.createElement("li");
+  li.innerText = text;
+  li.className =
+    "bg-gray-200 px-3 py-2 font-semibold rounded mb-2 flex justify-between items-center";
+
+  const deleteButton = document.createElement("button");
+  deleteButton.innerHTML =
+    "<img src='./delete.png' alt='Delete' class='w-8 h-8' />";
+  deleteButton.className = "";
+  deleteButton.addEventListener("click", (e) => {
+    li.remove();
+    updateTodoBottom();
+  });
+  li.appendChild(deleteButton);
+
+  list.appendChild(li);
+  inputElement.value = "";
+  inputElement.focus();
+
+  updateTodoBottom();
+}
+
+function updateTodoBottom() {
+  const numberOfTodos = list.childNodes.length;
+
+  const pendingTask = `You have ${numberOfTodos} pending task.`;
+
+  const p = document.createElement("p");
+  p.innerText = pendingTask;
+
+  const clearButton = document.createElement("button");
+  clearButton.className =
+    "px-4 py-2  flex justify-center items-center bg-purple-500 text-white rounded-md";
+  clearButton.innerText = "Clear All";
+  clearButton.addEventListener("click", () => {
+    list.innerHTML = "";
+    todoBottom.classList.add("hidden");
+  });
+
+  todoBottom.innerHTML = "";
+  todoBottom.classList.remove("hidden");
+  todoBottom.appendChild(p);
+  todoBottom.appendChild(clearButton);
+}
